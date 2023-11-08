b1275d977252e80a8029b4ea1385bf67af60daab
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-08 14:22:13 +0100

    add: thumbs images and action on current slide thumb image

--- js/script.js
+++ js/script.js
@@ -35,14 +35,21 @@ const wrapper = document.querySelector('.container');
 
 const sliderItems = sliderElementInDOM(sliderElems);
 // console.log(sliderItems);
+const sliderThumbs = sliderThumbsInDOM(sliderElems);
+// console.log(sliderThumbs);
 
 wrapper.append(sliderItems);
 
+// CREO ARRAY DI OGGETTI HTML PER ITEMS E THUMBS
 const items = document.querySelectorAll('.item');
 console.log(items, items[0]);
-
 items[0].classList.add('active');
 
+const thumbs = document.querySelectorAll('.thumb');
+console.log(thumbs, thumbs[0]);
+thumbs[0].classList.add('active');
+
+// CREO BOTTONI
 const prev = document.createElement('div');
 prev.classList.add('prev');
 sliderItems.append(prev);
@@ -51,10 +58,66 @@ const next = document.createElement('div');
 next.classList.add('next');
 sliderItems.append(next);
 
+// INIZIALIZZO INDICE DI POSIZIONE ED EVENT LISTENER CON FUNZIONI DI CALLBACK
+let curPosition = 0;
 prev.addEventListener("click", handlePrevious)
 next.addEventListener("click", handleNext)
 
-let curPosition = 0;
+
+
+// FUNZIONI
+/**
+ * Dato un array in ingresso con immagini e titoli per un carosello immagini esce un contenuto HTML pronto. 
+ * @param {array} array
+ * @returns {html object}
+*/
+function sliderElementInDOM(array) {
+    
+    let result = "";
+    
+    const items = document.querySelector('.items');
+    // console.log(items);
+    
+    array.forEach((curElem) => {
+        result += `
+        <div class="item">
+        <img src="${curElem.image}" alt="">
+        <div class="text">
+        <h3>${curElem.title}</h3>
+        <p>${curElem.caption}</p>
+        </div>
+        </div>
+        `;
+        
+    });
+    
+    items.innerHTML = result;
+    
+    return items;
+    
+}
+
+
+function sliderThumbsInDOM(array) {
+    
+    let thumbResult = "";
+    
+    const thumbs = document.querySelector('.thumbs');
+    
+    array.forEach((curElem) => {
+        thumbResult += `
+        <div class="thumb">
+        <img src="${curElem.image}" alt="">
+        </div>
+        `;
+        
+    });
+
+    thumbs.innerHTML = thumbResult;
+
+    return thumbs;
+}
+
 
 function handleNext() {
 
@@ -62,13 +125,17 @@ function handleNext() {
     for (let i = curPosition; i < items.length; i++) {
 
         const curItem = items[i];
+        const curThumb = thumbs[i];
 
         if (curItem.classList == "item active" && curItem == items[4]) {
 
             const nextItem = items[0];
+            const nextThumb = thumbs[0]
 
             curItem.classList.remove('active');
             nextItem.classList.add('active');
+            curThumb.classList.remove('active');
+            nextThumb.classList.add('active');
 
             curPosition = 0;
             break
@@ -77,9 +144,12 @@ function handleNext() {
         else if (curItem.classList == "item active") {
 
             const nextItem = items[i + 1];
+            const nextThumb = thumbs[i + 1];
 
             curItem.classList.remove('active');
             nextItem.classList.add('active');
+            curThumb.classList.remove('active');
+            nextThumb.classList.add('active');
 
             ++curPosition;
             break
@@ -96,13 +166,17 @@ function handlePrevious() {
     for (let i = curPosition; i < items.length; i--) {
 
         const curItem = items[i];
+        const curThumb = thumbs[i];
 
         if (curItem.classList == "item active" && curItem == items[0]) {
 
             const prevItem = items[4];
+            const prevThumb = thumbs[4];
 
             curItem.classList.remove('active');
             prevItem.classList.add('active');
+            curThumb.classList.remove('active');
+            prevThumb.classList.add('active');
 
             curPosition = 4;
             break
@@ -111,9 +185,12 @@ function handlePrevious() {
         else if (curItem.classList == "item active") {
 
             const prevItem = items[i - 1];
+            const prevThumb = thumbs[i - 1];
 
             curItem.classList.remove('active');
             prevItem.classList.add('active');
+            curThumb.classList.remove('active');
+            prevThumb.classList.add('active');
 
             --curPosition;
             break
@@ -122,36 +199,3 @@ function handlePrevious() {
     }
 
 }
-
-
-/**
- * Dato un array in ingresso con immagini e titoli per un carosello immagini esce un contenuto HTML pronto. 
- * @param {array} array
- * @returns {html object}
- */
-function sliderElementInDOM(array) {
-
-    let result = "";
-
-    const items = document.querySelector('.items');
-    // console.log(items);
-
-    array.forEach((curElem) => {
-        result += `
-        <div class="item">
-            <img src="${curElem.image}" alt="">
-            <div class="text">
-                <h3>${curElem.title}</h3>
-                <p>${curElem.caption}</p>
-            </div>
-        </div>
-        `;
-
-    });
-
-    items.innerHTML = result;
-
-    return items;
-
-}
-
